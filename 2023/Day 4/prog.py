4617bab9eec189e851232fc0abf0ab0cdd6656eb
Laozey <Qepron@hotmail.fr>
2023-12-04 12:26:48 +0100

    Day 4 puzzle completion

---
+++ 2023/Day 4/prog.py
@@ -0,0 +1,53 @@
+import re
+import numpy as np
+
+TEST_CASE = [
+    "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", #4
+    "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", #2
+    "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", #2
+    "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", #1
+    "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", #0
+    "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"  #0
+]
+
+def get_winning_match(card: str) -> int:
+    gotten, winning = card[9:].split('|')
+    gotten = [int(e) for e in re.findall(r'\d+', gotten)]
+    gotten.sort()
+    winning = [int(e) for e in re.findall(r'\d+', winning)]
+    winning.sort()
+
+    i = 0
+    j = 0
+    w = 0
+    while(i < len(gotten) and j < len(winning)):
+        if gotten[i] == winning[j]:
+            w += 1
+        if gotten[i] > winning[j]:
+            j += 1
+        else:
+            i += 1
+    
+    return w
+
+def compute_card_score(cards: list) -> int:
+    sum = 0
+    for c in cards:
+        m = get_winning_match(c)
+        sum += 2**(m-1) if m else 0
+    return sum
+
+def compute_card_count(cards: list) -> int:
+    cards_count = np.array([1] * len(cards))
+    for i, c in enumerate(cards):
+        for m in range(get_winning_match(c)):
+            cards_count[i+1+m] += cards_count[i]
+    return cards_count
+
+
+with open('d:/Programming/Misc/AdventOfCode/2023/Day 4/input.txt', 'r') as f:
+    cards = f.readlines()
+    # Part one
+    print(compute_card_score(cards))
+    # Part two
+    print(sum(compute_card_count(cards)))
